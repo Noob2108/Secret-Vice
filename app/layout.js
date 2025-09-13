ad4c20ae6b4915c2e30d07fe78bb46512e16a94c
Just-Beee <231191061+Just-Beee@users.noreply.github.com>
2025-09-13 17:23:41 +0930

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,21 @@
 import "./globals.css";
 import Header from "./components/Header";
+import BackgroundLogo from "./components/BackgroundLogo";
+
+export const metadata = {
+  title: "Secret Vice Automapper",
+  description: "Beat Saber automapper by Secret Vice",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body className="bg-deep text-white" style={{ minHeight: "100vh", position: "relative" }}>
+        <BackgroundLogo />
         <Header />
-        {children}
+        <main style={{ position: "relative", zIndex: 10 }}>
+          {children}
+        </main>
       </body>
     </html>
   );
